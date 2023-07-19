9dff617813118778fec2dc1c423f5cec53aa171c
Yacin Bahi <yacin@red64.io>
2023-07-19 16:30:09 +0100

    /brucelee/solana/5 returns dyn attributes

--- server/app.js
+++ server/app.js
@@ -1,15 +1,29 @@
 
 const express = require('express');
 const path = require("path");
+const axios = require('axios')
+
+const baseUrl = 'https://test-api.byte.city';
+const url = baseUrl+'/nftdata/show'
 
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.get('/:type/:chain/:id', function(req, res) {
+  if (['GET'].includes(req.method) && !req.is('application/json')) {
+    axios
+    .post(url, {
+      TokenId: parseInt(req.params.id), 
+      Market: req.params.chain, 
+      Series: req.params.type
+    })
+    .then((response) => { console.log(response.data); res.send(response.data); })  
+    .catch((err) => { console.log(err) })
+  } else {
+    next();
+  }
 });
 
 // All other GET requests not handled before will return our React app

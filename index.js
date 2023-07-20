15b55aa46cbb101673fa3c4f8f74a57b5db6b088
Francesco Sullo <sullof@gmail.com>
2023-07-19 17:33:10 -0700

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./server/app");
 
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3304;
 
 app.listen(port, () => {
   console.info("Listening to", port, process.env.NODE_ENV);

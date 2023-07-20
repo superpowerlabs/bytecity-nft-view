91ae19b25b569fb31b2d2b5eb9eaa227ca098ffb
Yacin Bahi <yacin@red64.io>
2023-07-20 01:11:36 +0100

    fixing routes

--- server/app.js
+++ server/app.js
@@ -11,24 +11,54 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../build')));
 
-app.get('/:type/:chain/:id', function(req, res) {
-  if (['GET'].includes(req.method) && !req.is('application/json')) {
+// Handle GET requests to /api route
+app.get('/:type/:chain/:id', function(req, res, next) {
+  const contentType = req.headers['content-type'];
+  if (/json/.test(contentType)) {
     axios
-    .post(url, {
-      TokenId: parseInt(req.params.id), 
-      Market: req.params.chain, 
-      Series: req.params.type
-    })
-    .then((response) => { res.send(response.data); })  
-    .catch((err) => { console.log(err) })
-  } else {
+      .post(url, {
+        TokenId: parseInt(req.params.id), 
+        Market: req.params.chain, 
+        Series: req.params.type
+      })
+      .then((response) => { res.send(response.data.data); })
+      .catch(() => { res.send({
+        success: false,
+        error: "Metadata not found"
+      }); 
+    });
+  }
+  else {
     next();
   }
 });
 
+app.use("/:anything", function (req, res, next) {
+  console.log("serve static:", req.params)
+  let v = req.params.anything;
+  if (!/\.module.aswm$/.test(v)) {
+    switch (v) {
+      case "favicon.ico":
+      case "manifest.json":
+      case "logo192.png":
+      case "static":
+        break;
+      default:
+        console.log("isHome = true");
+        res.locals.isHome = true;
+    }
+  }
+  next();
+});
+
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+app.get('*', (req, res, next) => {
+  console.log("serve index.html:", req.params);
+  if (res.locals.isHome) {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  } else {
+    next();
+  }
 });
 
 module.exports = app;

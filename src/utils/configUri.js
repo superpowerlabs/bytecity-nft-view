283f95d73e7a103f95f4bdee77173655cbb01d8c
woodxzw <85925484+woodxzw@users.noreply.github.com>
2023-07-21 17:20:19 +0800

    Update configUri.js

--- src/utils/configUri.js
+++ src/utils/configUri.js
@@ -1,6 +1,6 @@
-const baseUrl = 'https://test-api.byte.city';
+// const baseUrl = 'https://test-api.byte.city';
 // const baseUrl = 'http://192.168.91.119:8096';
-// const baseUrl = 'https://byte-release-api.byte.city';
+const baseUrl = 'https://byte-release-api.byte.city';
 
 const url = {
     getData: baseUrl+'/nftdata/show',
